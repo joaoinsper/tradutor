59023be35206f470ea35920612b63ccdbc92ee3e
joaoinsper <joaovsb2@insper.edu.br>
2024-06-17 13:12:20 -0300

    testando historico

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,3 +1,14 @@
+let historico = [];
+
+function copyToClipboard() {
+    const traduzido = document.getElementById("traduzindo").innerText;
+    navigator.clipboard.writeText(traduzido).then(() => {
+        alert("Tradução copiada para a área de transferência!");
+    }).catch(err => {
+        console.error("Erro ao copiar: ", err);
+    });
+}
+
 function copyToClipboard() {
     const traduzido = document.getElementById("traduzindo").innerText;
     navigator.clipboard.writeText(traduzido).then(() => {
@@ -11,3 +22,4 @@ function updateCharCount() {
     const texto = document.getElementById("texto_a_traduzir").value;
     document.getElementById("char_count").innerText = "Caracteres: " + texto.length;
 }
+
