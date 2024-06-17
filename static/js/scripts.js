7578de006bc6bf2655c706adab9f34a5b24f82fc
joaoinsper <joaovsb2@insper.edu.br>
2024-06-17 12:59:44 -0300

    criação js

---
+++ static/js/scripts.js
@@ -0,0 +1,13 @@
+function copyToClipboard() {
+    const traduzido = document.getElementById("traduzindo").innerText;
+    navigator.clipboard.writeText(traduzido).then(() => {
+        alert("Tradução copiada para a área de transferência!");
+    }).catch(err => {
+        console.error("Erro ao copiar: ", err);
+    });
+}
+
+function updateCharCount() {
+    const texto = document.getElementById("texto_a_traduzir").value;
+    document.getElementById("char_count").innerText = "Caracteres: " + texto.length;
+}
